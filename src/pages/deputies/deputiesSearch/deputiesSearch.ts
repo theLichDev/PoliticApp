6fb8aec695d469dc56b0ae7bb3675196b90ab8e4
theLichDev <imanol94@gmail.com>
2017-07-29 22:24:04 +0200

    Added dynamic data for ion-select. Added Circunscripcion field for advanced search.

--- src/pages/deputies/deputiesSearch/deputiesSearch.ts
+++ src/pages/deputies/deputiesSearch/deputiesSearch.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ViewController } from 'ionic-angular';
+
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'deputies-search',
   templateUrl: 'deputiesSearch.html'
 })
-export class DeputiesSearchModal {
+export class DeputiesSearchModal implements OnInit{
 
   public search: Object = {};
+  public parliamentaryGroups: string[];
+  public districts: string[];
 
   constructor(
-    public viewCtrl: ViewController
+    public viewCtrl: ViewController,
+    public database: AngularFireDatabase
   ) {  }
+
+  ngOnInit() {
+    this.database.list('/parliamentaryGroups')
+      .subscribe((data: any[]) => {
+        this.parliamentaryGroups = data.map((item) => {
+          return item.$value;
+        });
+      });
+    this.database.list('/districts')
+      .subscribe((data: any[]) => {
+        this.districts = data.map((item) => {
+          return item.$value;
+        });
+      });
+  }
 
   closeSearch() {
     this.viewCtrl.dismiss();
