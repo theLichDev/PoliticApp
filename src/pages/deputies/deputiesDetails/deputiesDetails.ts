9a67b10f11d1dc6d72cb2be29dfad4d1c27534be
theLichDev <imanol94@gmail.com>
2017-08-04 18:58:24 +0200

    Dynamic socialNetworks UI.

--- src/pages/deputies/deputiesDetails/deputiesDetails.ts
+++ src/pages/deputies/deputiesDetails/deputiesDetails.ts
@@ -10,6 +10,7 @@ import { Deputy } from '../../../shared/models/deputy';
 export class DeputiesDetailsPage implements OnInit{
 
   public deputy: Deputy;
+  public socialNetworks: Array<{ name: string, url: string }>;
 
   constructor(
     public navCtrl: NavController,
@@ -17,7 +18,16 @@ export class DeputiesDetailsPage implements OnInit{
   ) {  }
 
   ngOnInit() {
-   this.deputy = this.navParams.data;
+    this.deputy = this.navParams.data;
+    if (this.deputy.socialNetworks) {
+      this.socialNetworks = Object.keys(this.deputy.socialNetworks).map((socialNetworkName) => {
+        return { name: socialNetworkName, url: this.deputy.socialNetworks[socialNetworkName] };
+      });
+    }
+  }
+
+  openInAppWeb(url: string) {
+    console.log(url);
   }
 
 }
