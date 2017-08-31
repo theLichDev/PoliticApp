2fc14aa8fc3d61ab324e0a59e23169961115ff58
theLichDev <imanol94@gmail.com>
2017-09-01 00:14:04 +0200

    Added labels for description and members

--- src/pages/governmentEntities/entitiesDetails/entitiesDetails.ts
+++ src/pages/governmentEntities/entitiesDetails/entitiesDetails.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+
+import { DeputiesDetailsPage } from '../../deputies/deputiesDetails/deputiesDetails';
 import { Deputy } from '../../../shared/models/deputy';
 
 @Component({
@@ -14,11 +17,24 @@ export class EntitiesDetailsPage implements OnInit{
 
   constructor(
     public navCtrl: NavController,
-    private navParams: NavParams
+    private navParams: NavParams,
+    private database: AngularFireDatabase
   ) {  }
 
   ngOnInit() {
     this.governmentEntity = this.navParams.data;
+    if (this.governmentEntity.members) {
+      this.database.list('/deputies')
+      .subscribe((data: Deputy[]) => {
+        this.deputies = data.filter((deputy) => {
+          return this.governmentEntity.members.indexOf(deputy.id) > -1;
+        });
+      });
+    }
+  }
+
+  deputyDetails(deputy: Deputy) {
+    this.navCtrl.push( DeputiesDetailsPage, deputy);
   }
 
 }
